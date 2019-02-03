0072a910525cdae5e44842ce94c4365afecb9596
VladislavMelnic <47299064+VladislavMelnic@users.noreply.github.com>
2019-02-03 19:07:34 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+//import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        random rand = new random();
+        String[] list = new String[3];
+        list[0] = "Фильм: Начало";
+        list[1] = "Фильм: Время";
+        list[2] = "Фильм: Как украсть небоскрёб";
+
+        System.out.println("Если хотите продолжить введите 1");
+        System.out.println("Если хотите продолжить введите любое другое число");
+        int selection = sc.nextInt();
+
+        //while (selection==1){
+          for (;selection==1;){
+              rand.Random_number(0, 3);
+            if (rand.random_number1 == 0) {
+                System.out.println(list[0]);
+                System.out.println("Если хотите продолжить введите 1");
+                System.out.println("Если хотите продолжить введите любое другое число");
+                 selection = sc.nextInt();
+
+            }
+            else if (rand.random_number1 == 1) {
+                System.out.println(list[1]);
+                System.out.println("Если хотите продолжить введите 1");
+                System.out.println("Если хотите продолжить введите любое другое число");
+                selection = sc.nextInt();
+            }
+            else{
+                System.out.println(list[2]);
+                System.out.println("Если хотите продолжить введите 1");
+                System.out.println("Если хотите продолжить введите любое другое число");
+                selection = sc.nextInt();
+            }
+        }
+         System.out.println("Спасибо что были с нами");
+    }
+
+}
---
+++ random.java
@@ -0,0 +1,14 @@
+public class random {
+ public  int random_number1;
+    public void  Random_number (int a,int b){
+
+        random_number1 = a + (int) (Math.random() * b);
+     //System.out.println("1-ое случайное число: " + random_number1);
+ }
+   public int getrandom_number1(){
+        return random_number1;
+    }
+
+
+
+}
